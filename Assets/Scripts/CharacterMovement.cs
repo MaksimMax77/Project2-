9e0d924b71aacd86d9e086ca06cc3f7b3145326b
Den <dabramov@starlabstudio.com>
2019-09-05 01:32:59 +0400

    Added character movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Vector2 vecocity;
+    public float speed = 0.0F;
+
+    void Update()
+    {
+        transform.position += (Vector3) vecocity.normalized * Time.deltaTime * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterMovement _characterMovement;
+
+    void Awake()
+    {
+        _characterMovement = GetComponent<CharacterMovement>();
+    }
+
+    void Update()
+    {
+        _characterMovement.vecocity = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"));
+    }
+}
