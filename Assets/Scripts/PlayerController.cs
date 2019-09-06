2f906b9f61f724e0da65f419e31da5b7b14fe662
satanhead77 <krolova1965@gmail.com>
2019-09-06 19:47:16 +0300

    Кнопка атаки

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterMovement _characterMovement;
+    bool CanDamage;//если тру то можно нанести типо урон 
 
-    void Awake()
+	[SerializeField] private KeyCode _AttackButton = KeyCode.F;//кнопка которую можно менять в инспекторе
+
+	void Awake()
     {
-        _characterMovement = GetComponent<CharacterMovement>();
+        _characterMovement = GetComponent<CharacterMovement>(); 
     }
 
     void Update()
@@ -16,5 +19,47 @@ public class PlayerController : MonoBehaviour
         _characterMovement.vecocity = new Vector2(
             Input.GetAxis("Horizontal"),
             Input.GetAxis("Vertical"));
+		    Attack(_AttackButton);  
     }
+
+
+	#region//метод атаки
+	/// <summary>
+	/// метод атаки
+	/// </summary>
+	/// <param name="кнопка удара"></param>
+	void Attack(KeyCode  AttackButton)
+	{
+		 AttackButton = _AttackButton;
+		if (Input.GetKeyDown(AttackButton))
+		{
+
+			if (CanDamage)
+			{
+				Debug.Log("ударил по врагу");
+			}
+			else
+			{
+				Debug.Log("ударил мимо");
+			}
+		}
+	}
+	#endregion
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Enemy")
+		{
+			CanDamage = true;
+	    }
+	}
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Enemy")
+		{
+			CanDamage = false;
+		}
+	}
 }
+	 
+ 
